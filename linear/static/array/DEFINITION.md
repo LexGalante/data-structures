306f720596ebd723f40413fb9fbae30070d1e0b6
Alex Volnei Galante <alex_gm_100@hotmail.com>
2024-02-20 16:46:04 -0300

    array

---
+++ linear/static/array/DEFINITION.md
@@ -0,0 +1,19 @@
+## Definição
+Coleção de items do mesmo tipo, alocados em sequencia na memoria, um tipo de estrutura de dados simples e leve, tipo base para diversas listas.
+
+[Documentação](https://www.geeksforgeeks.org/array-data-structure/)
+
+### Conceitos Importantes
+- INDEX trata-se do número da posição de um valor em um array
+- ELEMENT trata-se do proprio elemento em si, int, string, decimal etc...
+- LENGTH trata-se do tamanho da lista
+
+### Prós
+- Leve
+- Fácil para busca por por posição
+
+### Contra
+- Mais custoso para adicionar um item/alterar/remover um item devido a necessidade de resizing do mesmo
+
+### Casos de Uso
+- Armazenar itens em sequencia em que se tem conhecimento prévio do tamanho do array
---
+++ linear/static/array/Program.cs
@@ -0,0 +1,46 @@
+﻿// opções para inicializa o array
+string[] languages = ["dotnet", "golang", "python"];
+
+languages = new string[] {"dotnet", "golang", "python"};
+
+void PrintValues(string name, object[] items)
+{
+    Console.WriteLine($"print all value for collection: {name}");
+    foreach (var item in items)
+        Console.WriteLine($"value: {item}");    
+}
+
+PrintValues("array with language names", languages);
+
+// acessar um valor pelo indice do array
+var numbers = new int[6] { 5, 8, 99, 441, 3, 1112125 };
+
+PrintValues("array with integer values", numbers.Select(x => x.ToString()).ToArray());
+
+// busca pelo indice
+Console.WriteLine($"numbers[2] = {numbers[2]}");
+
+// busca binária de um elemento
+Console.WriteLine($"bynary search number 3 in array [5, 8, 99, 441, 3, 1112125] = {Array.BinarySearch(numbers, 441)}");
+
+// array's bidimensionais
+int[,] array2DInitialization =  
+{
+    { 1, 2 },
+    { 3, 4 }, 
+    { 5, 6 },
+    { 7, 8 }
+};
+
+int[,,] array3D = new int[,,] 
+{
+    { 
+        { 1, 2, 3 }, 
+        { 4,   5,  6 } 
+    },
+    {
+        { 7, 8, 9 },
+        { 10, 11, 12 } 
+    } 
+};
+
